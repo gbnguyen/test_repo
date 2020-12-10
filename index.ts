e636887cf8d822e44d31544cc30c613008b9e90d
Benoit Fournier <benoit.fournier@sage.com>
2020-12-10 19:45:35 +0100

    addiing req.params

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ const PORT : string|number = process.env.PORT || 5000;
 
 app.use("*",(req, res) =>{
 
-    
-
     res.send(`<h1>Welcome to your simple server! Awesome right</h1> ${JSON.stringify(req.params,null,1)}`);
+
+    res.send(`req.query : ${JSON.stringify(req.query)}`);
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
