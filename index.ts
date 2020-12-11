862f075b3041d06eb89af04463c688421e8f1402
Benoit Fournier <benoit.fournier@sage.com>
2020-12-11 17:11:51 +0100

    req

--- index.ts
+++ index.ts
@@ -2,20 +2,19 @@ import express from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
+
+
 app.use("*",(req, res) =>{
 
     res.send(`<h1>Welcome to your simple server! Awesome right</h1> ${JSON.stringify(req.params,null,1)}`);
 
     console.log(`req.query : ${JSON.stringify(req.query)}</p>`);
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-
-    console.log('<p>');
-    urlParams.forEach((value:string,key:string)=>{
-        console.log(`<br/> ${value} - ${key}`);
-    })
-    console.log('</p>');
+   console.log(`req.params : ${req.params}`);
+   console.log(`req.body : ${req.body}`);
+   console.log(`req.params : ${req.body}`);
+   console.log(`req.query : ${req.query}`);
+   console.log(`req.query : ${req.headers}`);
 
 });
 
