23288ace77b3f80e0c71f1ac34d04e1a2e0a7c8c
Benoit Fournier <benoit.fournier@sage.com>
2020-12-11 16:47:03 +0100

    getPostValues

--- index.ts
+++ index.ts
@@ -6,7 +6,17 @@ app.use("*",(req, res) =>{
 
     res.send(`<h1>Welcome to your simple server! Awesome right</h1> ${JSON.stringify(req.params,null,1)}`);
 
-    res.send(`req.query : ${JSON.stringify(req.query)}`);
+    res.send(`<p>req.query : ${JSON.stringify(req.query)}</p>`);
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    res.send('<p>');
+    urlParams.forEach((value:string,key:string)=>{
+        res.send(`<br/> ${value} - ${key}`);
+    })
+    res.send('</p>');
+
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
